36bd62328a86e8a87e69a59338b241c64ae05560
cardyGPT <cardyGPT@cardyai.com>
2026-03-03 23:32:49 +0530

    feat(PICS-1463): add backend/src/main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,22 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-
-  const config = new DocumentBuilder()
-    .setTitle('Cardinality i18N Engine')
-    .setDescription('Automated Translation API')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
-
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
