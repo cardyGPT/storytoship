b67af3e3473152772df03fddd8a9896900cfdb22
cardyGPT <cardyGPT@cardyai.com>
2026-03-04 11:34:35 +0530

    feat(PICS-1762): add frontend/src/app/app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,28 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { RbacMenuComponent } from './rbac/rbac-menu.component';
-import { RbacMenuLoader } from './core/rbac-menu.loader';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new RbacMenuLoader(http);
-}
 
 @NgModule({
-  declarations: [AppComponent, RbacMenuComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    })
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
