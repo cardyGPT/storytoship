2e11bc5c2a884a59d331b8c6c49f637f8a6b840b
cardyGPT <cardyGPT@cardyai.com>
2026-03-23 13:32:57 +0530

    feat(PICS-1492): add frontend/src/app/app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,32 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { HelpService } from './services/help.service';
+import { LocalizationService } from './services/localization.service';
 
 @Component({
   selector: 'app-root',
   template: `
-    <nav>
-      <h1>Cardinality Docs POC</h1>
-      <button (click)="toggleHelp()">Need Help?</button>
-    </nav>
-    <main>
-      <router-outlet></router-outlet>
-      <button (click)="openContextualHelp('ops-guide')">Context Help: Operations</button>
-    </main>
-    <app-knowledge-sidebar></app-knowledge-sidebar>
+    <div class="container">
+      <nav>
+        <button (click)="switchLang('en')">English</button>
+        <button (click)="switchLang('de')">Deutsch</button>
+      </nav>
+
+      <div *ngIf="formConfig">
+        <formio 
+          [form]="formConfig" 
+          [options]="formOptions">
+        </formio>
+      </div>
+    </div>
   `
 })
 export class AppComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private helpService: HelpService) {}
-
-  ngOnInit() {
-    this.route.queryParams.subscribe(params => {
-      if (params['docId']) {
-        this.helpService.openHelp(params['docId']);
+  formConfig = {
+    components: [
+      {
+        type: 'textfield',
+        key: 'firstName',
+        label: 'firstName', // Key for i18n
+        placeholder: 'Enter first name',
+        validate: { required: true }
+      },
+      {
+        type: 'button',
+        action: 'submit',
+        label: 'submit',
+        theme: 'primary'
       }
+    ]
+  };
+
+  formOptions: any = {};
+
+  constructor(private locService: LocalizationService) {}
+
+  async ngOnInit() {
+    await this.locService.init();
+    this.updateFormOptions();
+
+    this.locService.currentLang$.subscribe(() => {
+      this.updateFormOptions();
     });
   }
 
-  toggleHelp() { this.helpService.openHelp(); }
-  openContextualHelp(slug: string) { this.helpService.openHelp(slug); }
+  updateFormOptions() {
+    this.formOptions = {
+      i18n: this.locService.getI18nOptions()
+    };
+  }
+
+  switchLang(lang: string) {
+    this.locService.changeLanguage(lang);
+  }
 }
