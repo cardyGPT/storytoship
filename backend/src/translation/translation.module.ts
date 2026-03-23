2f3a86c18e06d2348d24d7f943daf24da880c2ba
cardyGPT <cardyGPT@cardyai.com>
2026-03-23 13:32:54 +0530

    feat(PICS-1492): add backend/src/translation/translation.module.ts

--- backend/src/translation/translation.module.ts
+++ backend/src/translation/translation.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { TranslationController } from './translation.controller';
-import { TranslationService } from './translation.service';
-import { Translation } from './entities/translation.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Translation])],
   controllers: [TranslationController],
-  providers: [TranslationService],
-  exports: [TranslationService],
 })
 export class TranslationModule {}
